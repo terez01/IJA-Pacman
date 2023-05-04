4eeae715178de6263ca397edea26f2a660a3871f
Knowny <67062749+Knowny@users.noreply.github.com>
2023-05-05 01:33:45 +0200

    changing the structure

---
+++ src/main/java/ija/proj/pacman/Main.java
@@ -0,0 +1,45 @@
+package ija.proj.pacman;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        BorderPane layout = new BorderPane();
+
+        MenuBar menu = new MenuBar();
+        Menu play = new Menu("Play");
+        Menu map = new Menu("Maps");
+        Menu log = new Menu("Playback");
+
+        menu.getMenus().addAll(play, map, log);
+
+        RadioMenuItem item1 = new RadioMenuItem("Map 1");
+        RadioMenuItem item2 = new RadioMenuItem("Map 2");
+        RadioMenuItem item3 = new RadioMenuItem("Map 3");
+
+        ToggleGroup rGroup = new ToggleGroup();
+        rGroup.getToggles().addAll(item1, item2, item3);
+
+        map.getItems().addAll(item1, item2, item3);
+
+        layout.setTop(menu);
+        Scene scene = new Scene(layout, 320, 240);
+        stage.setTitle("Pac-Man");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
