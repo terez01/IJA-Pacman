eda71251a91b5db8a46c8837ba7929547c63ca74
Knowny <67062749+Knowny@users.noreply.github.com>
2023-05-05 01:57:51 +0200

    adding the field, maze and maze object interfaces

---
+++ src/main/java/ija/proj/pacman/common/Field.java
@@ -0,0 +1,18 @@
+/**
+ * IJA - PACMAN
+ * author(s): xmager00, xhusar11
+ */
+package ija.proj.pacman.common;
+
+public interface Field {
+    enum Direction{
+        U, L, D, R
+    }
+    Field nextField(Field.Direction dir);
+    boolean canMove();
+    boolean isEmpty();
+    MazeObject get();
+    boolean put(MazeObject object);
+    boolean remove(MazeObject object);
+    void setMaze(Maze maze);
+}
